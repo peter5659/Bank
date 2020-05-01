75b2d59a8f2fa5244d2eb6b3a42d5f50a6e3ff0e
peter5659 <peter5659@kau.kr>
2020-05-01 18:03:50 +0900

    처음화면

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 from Account import *
+
 #a=Account("123","jon",30000) 이런식으로..
 def show_menu():
     print("======Bank Menu======")
@@ -9,22 +11,27 @@ def show_menu():
     print("5. 종료하기")
     print("=====================")
 
+account_objects=[]
 while(1):
     show_menu()
     menu=int(input())
     if(menu==1):
-        count=0
+        
         print("======계좌개설======")
         account_number=str(input("계좌번호 :"))
         name=str(input("이름: "))
         first_money=int(input("예금: "))
+        
         a=Account(account_number,name,first_money)
+        account_objects.append(a)
+        
         print("##계좌개설을 완료하였습니다##")
 
     # elif(menu==str(2)):
     # elif(menu==str(3)):
     elif(menu==4):
-        a.introduce()
+        for i in range(len(account_objects)):
+            account_objects[i].introduce()
         
     elif(menu==5):
         print("##프로그램을 종료합니다##")
