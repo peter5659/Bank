74d2526cb89a098554d5f2c1a1c758c64563cae7
unknown <1p2h3s@naver.com>
2020-05-03 20:10:55 +0900

    umm

--- main(hwan).py
+++ main(hwan).py
@@ -1,25 +1,4 @@
 from Account import *
-
-class System(Account):
-    def deposit(self):
-                
-        
-        self.introduce()
-        money_in=int(input("입금하실 금액을 입력해주세요: "))
-        self.money+=money_in
-        self.introduce()
-    
-    def withdraw(self):
-                
-        self.introduce()
-        money_out=int(input("출금하실 금액을 입력해주세요: "))
-        if money_out>self.money:
-            print("돈이 부족합니다")
-        else:
-            self.money-=money_out
-        self.introduce()
-
-    
 class BankingSystem:
 #a=Account("123","jon",30000) 이런식으로..
     def show_menu():
@@ -53,7 +32,7 @@ class BankingSystem:
             name=str(input("이름: "))
             first_money=int(input("예금: "))
             
-            a=System(account_number,name,first_money)
+            a=Account(account_number,name,first_money)
             account_objects.append(a)
             confirm[account_number]=num
             num+=1
@@ -66,8 +45,16 @@ class BankingSystem:
                 if find in confirm:
                     break
                 print("존재하지 않는 계좌번호입니다.")
-            account_objects[confirm[find]].deposit()
+                    
+            for i in range(1):
+                account_objects[confirm[find]].introduce()
+            money_in=int(input("입금하실 금액을 입력해주세요: "))
 
+
+            account_objects[confirm[find]].money+=money_in
+            i=confirm[find]
+            for i in range(1):
+                account_objects[confirm[find]].introduce()
 
         elif(menu==str(3)):
             while(1):
@@ -75,8 +62,16 @@ class BankingSystem:
                 if find in confirm:
                     break
                 print("존재하지 않는 계좌번호입니다.")
-                account_objects[confirm[find]].withdraw()
-
+            for i in range(1):
+                account_objects[confirm[find]].introduce()
+            money_out=int(input("출금하실 금액을 입력해주세요: "))
+            if money_out>account_objects[confirm[find]].money:
+                print("돈이 부족합니다")
+            else:
+                account_objects[confirm[find]].money-=money_out
+            i=confirm[find]
+            for i in range(1):
+                account_objects[confirm[find]].introduce()
 
         elif(menu==str(4)):
             for i in range(len(account_objects)):
